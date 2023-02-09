9b809ad1c9ca70087570bdb675b1023e20e72242
theVerySharpFlat <aidenramen29@gmail.com>
2023-02-09 15:43:04 -0600

    basic windowing support

--- sandbox/sandbox.c
+++ sandbox/sandbox.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+
 #include <spritz.h>
+#include <spritz_window.h>
 
 int main() {
-    sayHello();
+    SpritzWindow_t window = spritzCreateWindow(680, 420, "Spritz!");
+
+    while(!spritzWindowShouldClose(window)) {
+        if(spritzIsKeyPressed(window, SPRITZ_KEY_Q)) {
+            printf("Q was pressed!\n");
+        } else {
+            printf("Q was not pressed!\n");
+        }
+
+        spritzUpdateWindows();
+    }
 }
---
+++ spritz/src/spritz_window_internal.h
@@ -0,0 +1,10 @@
+#ifndef SPRITZ_WINDOW_INTERNAL_H
+#define SPRITZ_WINDOW_INTERNAL_H
+
+#include <GLFW/glfw3.h>
+
+struct SpritzWindow {
+    GLFWwindow* window;
+};
+
+#endif
